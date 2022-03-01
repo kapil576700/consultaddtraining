a4dae1d19eb06f87413e9ce66b3236baae5c192e
kapil576700 <kapil576700@gmail.com>
2022-02-28 23:17:36 -0500

    adding program10

--- task3/Program10.java
+++ task3/Program10.java
@@ -1,10 +1,30 @@
 package task3;
 
+import java.util.Scanner;
+
 public class Program10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of lines: ");
+		int line= sc.nextInt();
+		int space = line;
+		int num=1;
+		
+		for(int i=0; i<line;i++) {
+			for(int s=1; s<space;s++) {
+				System.out.print(" ");
+			}
+			num=1;
+			for(int j=0; j<=i;j++) {
+				System.out.print(num+" ");
+				num=num*(i-j)/(j+1);
+			}
+			space--;
+			System.out.println();
+		}
+		
 	}
 
 }
